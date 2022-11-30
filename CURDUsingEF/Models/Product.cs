73f158b3ad7897899bb6689eb314c6034c6d63d3
Pradnyakore <pradnyakore99@gmail.com>
2022-11-30 14:48:22 +0530

    Add project files.

---
+++ CURDUsingEF/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CURDUsingEF.Models
+{
+    [Table("Productt")]
+    public class Product
+    {   
+       [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Company Name is Required")]
+        [Display(Name="Company Name")]
+        public string Company { get; set; }
+
+        [Required(ErrorMessage = "Price is Required")]
+        public int Price { get; set; }
+
+
+    }
+}
